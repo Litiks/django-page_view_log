6fe2375ce1ac45c67e8ca4a82a57c42d34c6c5e4
Aaron Mader <aaron@litiks.com>
2019-11-13 16:14:16 -0500

    performance improvements within the django admin.

--- page_view_log/admin.py
+++ page_view_log/admin.py
@@ -18,12 +18,31 @@ class ViewNameAdmin(admin.ModelAdmin):
     ordering = ('view_name_string',)
     list_display = ('view_name_hash', 'view_name_string')
 
+class StatusCodeFilter(admin.SimpleListFilter):
+    """ If we leave django to it's own devices; it tries to determine the unique values for this filter by querying the table (which is massive).
+        Instead, we only list those codes occurring recently.
+        see: https://docs.djangoproject.com/en/2.2/ref/contrib/admin/#django.contrib.admin.ModelAdmin.list_filter
+    """
+    title = 'status code'
+    parameter_name = 'status_code'
+
+    def lookups(self, request, model_admin):
+        # this is the magic
+        qs = model_admin.get_queryset(request).values_list('status_code', flat=True)[:1000]
+        qs = sorted(set(qs))
+        return [(n,n) for n in qs]
+
+    def queryset(self, request, queryset):
+        v = self.value()
+        if v:
+            return queryset.filter(status_code=v)
+
 class PageViewLogAdmin(admin.ModelAdmin):
     search_fields = ('user__email','ip_address','url__url_string')
-    ordering = ('-datetime',)
+    ordering = ('-id',)
 
     list_display = ('datetime', 'user', 'status_code','url', 'view_name','gen_time_in_milliseconds','ip_address', 'user_agent')
-    list_filter = ('status_code','view_name','user__email')
+    list_filter = (StatusCodeFilter,)
 
 admin.site.register(UserAgent, UserAgentAdmin)
 #admin.site.register(Url, UrlAdmin)
